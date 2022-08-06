1600c9e34291e896035cab5878fac31aec2774c9
saad fahad <101002016+ssaaddddsd@users.noreply.github.com>
2022-08-06 09:50:46 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
 
 from Shapes import *
-
-input1 =str(input("Write the name of the shape you want to try on :"))
-
-if input1 == "Square":
-    Square.Display()
-    num = int(input("\nEnter 1 to calculate the area or enter 2 to calculate the Perimeter :"))
-    if num == 1:
-        Square.Calculate_Area()
-    elif num == 2:
-        Square.Calculate_Perimeter()
-elif input1 == "Rectangle":
-    Rectangle.Display()
-    num = int(input("\nEnter 1 to calculate the area or enter 2 to calculate the Perimeter :"))
-    if num == 1:
-        area_rec = Rectangle.Calculate_area()
-        print(area_rec)
-    elif num == 2:
-         Rectangle.Calculate_perimeter()
-elif input1 == "Triangle":
-    Triangle.Display()
-    num = int(input("\nEnter 1 to calculate the area or enter 2 to calculate the Perimeter :"))
-    if num == 1:
+def saad():
+    input1 =str(input("Write the name of the shape you want to try on :"))
+    if input1 == "Square":
+        Square.Display()
+        num = int(input("\nEnter 1 to calculate the area or enter 2 to calculate the Perimeter :"))
+        if num == 1:
+          Square.Calculate_Area()
+        elif num == 2:
+         Square.Calculate_Perimeter()
+    elif input1 == "Rectangle":
+        Rectangle.Display()
+        num = int(input("\nEnter 1 to calculate the area or enter 2 to calculate the Perimeter :"))
+        if num == 1:
+            area_rec = Rectangle.Calculate_area()
+            print(area_rec)
+        elif num == 2:
+            Rectangle.Calculate_perimeter()
+    elif input1 == "Triangle":
+        Triangle.Display()
+        num = int(input("\nEnter 1 to calculate the area or enter 2 to calculate the Perimeter :"))
+        if num == 1:
          Triangle.calculate_area()
-    elif num == 2:
-        Triangle.calculate_perimeter()
-else:
-    print("Try agin")
+        elif num == 2:
+            Triangle.calculate_perimeter()
+    else:
+        print("Try agin")
+
+saad()
+wors = str(input("Do you want to restart the program to work in another form? \nSelect your answer with yes or no :"))
+while wors == "yes":
+    saad()
